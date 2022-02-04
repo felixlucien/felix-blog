9a39ec4b62863aee8e39e5c3e95b9905d33a55ad
Felix McCuaig <felixmccuaig@gmail.com>
2022-02-04 22:49:00 +1100

    Add cloudbuild and dockerfiles

--- packages/frontend/src/blog/BlogPosts.tsx
+++ packages/frontend/src/blog/BlogPosts.tsx
@@ -1,36 +1,45 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./BlogPosts.css";
 
 export const BlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    var posts = fetch("http://localhost:8080/listPosts")
+      .then((x) => x.json())
+      .then((x) => {
+        setPosts(x);
+      });
+  }, []);
+
   return (
     <div className="blog-posts">
       <h1>Blog Posts</h1>
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
-      <BlogTile />
+      {posts.map((x: any) => (
+        <BlogTile
+          key={x.title}
+          title={x.title}
+          description={x.description}
+          date={x.date}
+        />
+      ))}
     </div>
   );
 };
 
-const BlogTile = () => {
+type BlogTileProps = {
+  title: string;
+  description: string;
+  date: string;
+};
+
+const BlogTile = ({ title, description, date }: BlogTileProps) => {
   return (
     <Link to="blog/abcdefgh">
       <div className="blog-tile">
         <div className="blog-tile-header">
-          <h2>Monorepo like a boss with cloud run</h2>
+          <h2>{title}</h2>
           <h6
             style={{
               marginLeft: "auto",
@@ -39,7 +48,7 @@ const BlogTile = () => {
             11/12/2021
           </h6>
         </div>
-        <p>Description</p>
+        <p>{description}</p>
       </div>
     </Link>
   );
--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -2,11 +2,36 @@ import { Request, Response } from "express";
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+const cors = require("cors");
+
+app.use(cors({ origin: true }));
+
+app.get("/listPosts", (req: Request, res: Response) => {
+  var posts = [
+    {
+      title: "Monorepo like a boss with cloud build.",
+      description:
+        "Automatically build and deploy from your monorepo with cloud build",
+      date: new Date(),
+    },
+  ];
+
+  res.send(JSON.stringify(posts));
 });
+
+app.get("/config", (req: Request, res: Response) => {
+  res.send(
+    JSON.stringify({
+      env: process.env.ENV,
+      fav_color: process.env.FAV_COLOR,
+      fav_sport: process.env.FAV_SPORT,
+    })
+  );
+});
+
+app.post("/newPost", (req: Request, res: Response) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
